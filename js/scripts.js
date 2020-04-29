4b076e244b60d80cf88245db2a604c116e071106
Rohan Saxena <35458735+RhnSaxena@users.noreply.github.com>
2020-04-29 20:12:42 +0530

    Remove README.md

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,21 @@ myHeaders.append("x-api-key", "BiZrGMokNvaGtOLxHjURKV9ZbobVk6Zd");
 url = "https://api-dev.oninnovaccer.com/fhir/";
 var api = '';
 var query = '';
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
 
 function renderJson(endpoint, requestOptions){
     fetch(endpoint, requestOptions)
         .then(response => response.text())
-        .then(result => document.getElementById("json").innerHTML =JSON.stringify(JSON.parse(result), undefined, 4))
-        .then(result => document.getElementById("json").innerHTML =syntaxHighlight(result))
+        // .then(result => document.getElementById("json").innerHTML =JSON.stringify(JSON.parse(result), undefined, 4))
+        .then(result => document.getElementById("json").innerHTML =syntaxHighlight(JSON.parse(result)))
         .catch(error => document.getElementById("json").innerHTML =error);
 }
 
 function functionOne() {
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
 
     api = "Patient";
     query = "/ddf8e442-5b14-491d-80a4-adfffab4b3ea";
@@ -28,12 +27,6 @@ function functionOne() {
 
 function functionTwo() {
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
     api = "RelatedPerson";
     query = "/608809d0-0911-476c-aa89-53b2bdb98fe1";
 
@@ -42,12 +35,6 @@ function functionTwo() {
 
 function functionThree() {
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
     api = "Practitioner";
     query = "/1fdba303-cfa1-40c3-b058-d1b01ac8527b";
     
@@ -55,12 +42,6 @@ function functionThree() {
 }
 
 function functionFour() {
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
 
     api = "Medication";
     query = "?status=active";
@@ -71,7 +52,7 @@ function functionFour() {
 
 function syntaxHighlight(json) {
     if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
+         json = JSON.stringify(json, undefined, 4);
     }
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
