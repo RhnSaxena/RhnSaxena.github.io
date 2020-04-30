51f29f2a46bebaa4f577f0b5b6d420730cf4a652
Rohan Saxena <35458735+RhnSaxena@users.noreply.github.com>
2020-04-30 23:46:54 +0530

    UI Enhancements

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ function renderJson(id){
 
 function renderPatientList(Patients){
 
-    toggle_tabs("tab_1");
+    
     var text = "";
     text +="<table>"
     var patient;
@@ -35,18 +35,64 @@ function renderPatientList(Patients){
             + Patients["entry"][0]["resource"][patient]["gender"] + "<br>Address : "
             + Patients["entry"][0]["resource"][patient]["address"][0]["city"] + ", "
             + Patients["entry"][0]["resource"][patient]["address"][0]["state"] + "<br></td><td class='col-4'> "
-            + "<a class='more_info' onclick="+'renderJson('+'"'+ Patients["entry"][0]["resource"][patient]["id"]+'"' +')'+">More Info</a>"
-            + "</td></tr>" ;
+            + "<button class='btn btn-outline-danger' onclick="+'renderJson('+'"'+ Patients["entry"][0]["resource"][patient]["id"]+'"' +')'
+            +">More Info</button></td></tr>" ;
     }
     text +="</table>"
     document.getElementById("tab_1_json").innerHTML = text;
     document.getElementById("tab_1_json_des").innerHTML = "These are few instances of the chosen API.";
 }
 
+function renderAllergyList(Allergy){
+    
+    var text = "";
+    text +="<table>"
+    var allergy;
+    for (allergy in Allergy["entry"][0]["resource"]){
+        var cat;
+        text += "<tr> <td class='col-8'>Category : ";
+            for( cat in Allergy["entry"][0]["resource"][allergy]["category"]){
+                text+= Allergy["entry"][0]["resource"][allergy]["category"][cat] +", "
+            }
+        text = text.slice(0, -2);
+        text += "<br>Clinical Status : "
+                + Allergy["entry"][0]["resource"][allergy]["clinicalStatus"]["coding"][0]["code"]+ "<br>Patient : " 
+                + Allergy["entry"][0]["resource"][allergy]["patient"]["reference"]+"<br>"
+                 // + "Type : " +Allergy["entry"][0]["resource"][allergy]["encounter"]["type"] +"<br> " 
+                + "</td><td class='col-4'><button class='btn btn-outline-danger' onclick="
+                + 'renderJson('+'"'+ Allergy["entry"][0]["resource"][allergy]["id"]+'"' +')'
+                +">More Info</button></td></tr>" ;
+    }
+    text +="</table>"
+    document.getElementById("tab_1_json").innerHTML = text;
+    document.getElementById("tab_1_json_des").innerHTML = "These are few instances of the chosen API.";
+}
 
+function renderPractitionerList(Practitioner){
+    console.log(Practitioner)
+    console.log("Practitioner")
+    
+    var text = "";
+    text +="<table>"
+    var practitioner;
+    for (practitioner in Practitioner["entry"][0]["resource"]){
+
+        text += "<tr> <td class='col-8'>Name : "
+            + Practitioner["entry"][0]["resource"][practitioner]["name"][0]["given"][0] +" " 
+            + Practitioner["entry"][0]["resource"][practitioner]["name"][0]["family"] + "<br>Gender : "
+            + Practitioner["entry"][0]["resource"][practitioner]["gender"] + "<br>Address : "
+            + Practitioner["entry"][0]["resource"][practitioner]["address"][0]["city"] + ", "
+            + Practitioner["entry"][0]["resource"][practitioner]["address"][0]["state"] + "<br></td><td class='col-4'> "
+            + "<button class='btn btn-outline-danger' onclick="+'renderJson('+'"'+ Practitioner["entry"][0]["resource"][practitioner]["id"]+'"' +')'
+            +">More Info</button></td></tr>" ;
+    }
+    text +="</table>"
+    document.getElementById("tab_1_json").innerHTML = text;
+    document.getElementById("tab_1_json_des").innerHTML = "These are few instances of the chosen API.";
+}
 
 function function_Patient_Query() {
-
+    toggle_tabs("tab_1");
     api = "Patient";
     query = "?active=true";
     document.getElementById("tab_1_json").innerHTML = '';
@@ -57,33 +103,8 @@ function function_Patient_Query() {
         .catch(error => document.getElementById("tab_1_json").innerHTML =error);
 }
 
-
-function renderAllergyList(Allergy){
-    toggle_tabs("tab_1");
-    var text = "";
-    text +="<table>"
-    var allergy;
-    for (allergy in Allergy["entry"][0]["resource"]){
-        var cat;
-        text += "<tr> <td class='col-8'>Category : ";
-            for( cat in Allergy["entry"][0]["resource"][allergy]["category"]){
-                text+= Allergy["entry"][0]["resource"][allergy]["category"][cat] +", "
-            }
-        text += "<br>Clinical Status : "
-                + Allergy["entry"][0]["resource"][allergy]["clinicalStatus"]["coding"][0]["code"]+ "<br>"
-                 // + "Type : " +Allergy["entry"][0]["resource"][allergy]["encounter"]["type"] +"<br> " 
-                + "Patient : " +Allergy["entry"][0]["resource"][allergy]["patient"]["reference"] 
-                + "<br></td><td class='col-4'><a class='more_info' onclick="
-                + 'renderJson('+'"'+ Allergy["entry"][0]["resource"][allergy]["id"]+'"' +')'
-                +">More Info</a></td></tr>" ;
-    }
-    text +="</table>"
-    document.getElementById("tab_1_json").innerHTML = text;
-    document.getElementById("tab_1_json_des").innerHTML = "These are few instances of the chosen API.";
-}
-
 function function_AllergyIn_Query() {
-
+    toggle_tabs("tab_1");
     api = "AllergyIntolerance";
     query = "?clinical-status=active";
     document.getElementById("tab_1_json").innerHTML = '';
@@ -91,6 +112,18 @@ function function_AllergyIn_Query() {
         .then(response => response.text())
         // .then(result => document.getElementById("json").innerHTML =JSON.stringify(JSON.parse(result), undefined, 4))
         .then(result => renderAllergyList(JSON.parse(result)))
+        .catch(error => document.getElementById("tab_1_json").innerHTML =error);
+}
+
+function function_Practitioner_Query() {
+    toggle_tabs("tab_1");
+    api = "Practitioner";
+    query = "?active=true";
+    document.getElementById("tab_1_json").innerHTML = '';
+    fetch(url+api+query, requestOptions)
+        .then(response => response.text())
+        // .then(result => document.getElementById("json").innerHTML =JSON.stringify(JSON.parse(result), undefined, 4))
+        .then(result => renderPractitionerList(JSON.parse(result)))
         .catch(error => document.getElementById("tab_1_json").innerHTML =error);
 }
 
@@ -116,16 +149,12 @@ function syntaxHighlight(json) {
     });
 }
 
-
-
 function submit_app_token(){
-    var app_token = document.getElementById('app_token').value;
 
+    var app_token = document.getElementById('app_token').value;
     if(app_token==""){
         alert("Please Enter the app token");
-    }
-
-    else{
+    }else{
         myHeaders.append("x-api-key", app_token);
         document.getElementById('api_container').style.display='block';
         document.getElementById('tab_1').style.display='block';
